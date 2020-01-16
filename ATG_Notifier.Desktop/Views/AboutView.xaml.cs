be6b89e924defd5720cc0ee4cee1cc4cd2fefda8
Felix <FelixDev91@gmail.com>
2020-01-16 21:47:24 +0100

    The application name and version in the about dialog can now be copied.

--- ATG_Notifier.Desktop/Views/AboutView.xaml.cs
+++ ATG_Notifier.Desktop/Views/AboutView.xaml.cs
@@ -3,6 +3,7 @@ using ATG_Notifier.Desktop.Helpers;
 using ATG_Notifier.Desktop.Utilities;
 using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ATG_Notifier.Desktop.Views
 {
@@ -13,6 +14,9 @@ namespace ATG_Notifier.Desktop.Views
             InitializeComponent();
 
             this.Title = $"About {AppConfiguration.AppId}";
+
+            this.AppIdTextBox.Text = AppConfiguration.AppId;
+            this.AppVersionTextBox.Text = $"Version {AppConfiguration.AppVersion}";
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -28,6 +32,44 @@ namespace ATG_Notifier.Desktop.Views
         private void OnSourceCodeHyperlinkClick(object sender, RoutedEventArgs e)
         {
             WebUtility.OpenWebsite(AppConfiguration.SourceCodeUri);
+        }
+
+        private void OnAppIdTextBoxMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.AppIdTextBox.SelectAll();
+        }
+
+        private void OnAppVersionTextBoxMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.AppVersionTextBox.SelectAll();
+        }
+
+        private void OnAppIdTextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            ClearAppIdTextBoxSelection();
+        }
+
+        private void OnAppVersionTextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            ClearAppVersionTextBoxSelection();
+        }
+
+        private void OnGridMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ClearAppIdTextBoxSelection();
+            ClearAppVersionTextBoxSelection();
+        }
+
+        private void ClearAppIdTextBoxSelection()
+        {
+            this.AppIdTextBox.SelectionStart = 0;
+            this.AppIdTextBox.SelectionLength = 0;
+        }
+
+        private void ClearAppVersionTextBoxSelection()
+        {
+            this.AppVersionTextBox.SelectionStart = 0;
+            this.AppVersionTextBox.SelectionLength = 0;
         }
     }
 }
